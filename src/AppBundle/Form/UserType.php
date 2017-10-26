411e153a28a82db58075e5fa36fa0c9e12aea6b4
YuriyGreat <brandnewyriy97great@gmail.com>
2017-10-26 18:00:37 +0300

    added modules of registration. added doctrine logic

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -6,7 +6,18 @@
  * Time: 14.01
  */
 
-class UserType
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use AppBundle\Entity\User;
+
+class UserType extends AbstractType
 {
 
 }
